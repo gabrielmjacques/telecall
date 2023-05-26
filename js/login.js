2bec0e41c994a869f9374ea77b0cd0a327a042d8
gabrielmjacques <egmoreira10@gmail.com>
2023-05-26 00:59:08 -0300

    Textura da tela de login e animação de blur

--- js/login.js
+++ js/login.js
@@ -1,25 +1,22 @@
 login_div = document.getElementById( 'login_div' )
 cadastro_div = document.getElementById( 'cadastro_div' )
 
-function ShowCad()
-{
-    login_div.style.opacity = 0.1
+function ShowCad() {
+    login_div.style.filter = 'blur(10px)'
+    cadastro_div.style.filter = 'blur(0px)'
     cadastro_div.style.top = '0'
 }
 
-function HideCad()
-{
-    login_div.style.opacity = 1
+function HideCad() {
+    login_div.style.filter = 'blur(0px)'
+    cadastro_div.style.filter = 'blur(10px)'
     cadastro_div.style.top = '100%'
 }
 
-function ShowPassword( e )
-{
-    if ( e.type == 'text' )
-    {
+function ShowPassword( e ) {
+    if ( e.type == 'text' ) {
         e.setAttribute( 'type', 'password' )
-    } else
-    {
+    } else {
         e.setAttribute( 'type', 'text' )
     }
 }
