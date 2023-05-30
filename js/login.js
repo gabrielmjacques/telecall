104264d8d54f6e4a140a04c62dbd9fe3f903c21d
gabrielmjacques <egmoreira10@gmail.com>
2023-05-30 08:18:24 -0300

    Validacoes

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
-login_div = document.getElementById( 'login_div' )
-cadastro_div = document.getElementById( 'cadastro_div' )
+// Divs de cadastro e login
+const login_div = document.querySelector( '#login_div' )
+const cadastro_div = document.querySelector( '#cadastro_div' )
 
 function ShowCad() {
     login_div.style.filter = 'blur(10px)'
@@ -18,5 +19,18 @@ function ShowPassword( e ) {
         e.setAttribute( 'type', 'password' )
     } else {
         e.setAttribute( 'type', 'text' )
+    }
+}
+
+function ConfirmPasswordVerification() {
+    const new_password_entry = document.querySelector( '#new_password_entry' )
+    const confirm_password_entry = document.querySelector( '#confirm_password_entry' )
+
+    if ( confirm_password_entry.value == new_password_entry.value ) {
+        confirm_password_entry.classList.add( 'is-valid' )
+        confirm_password_entry.classList.remove( 'is-invalid' )
+    } else {
+        confirm_password_entry.classList.add( 'is-invalid' )
+        confirm_password_entry.classList.remove( 'is-valid' )
     }
 }
