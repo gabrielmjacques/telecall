90feff40ea71c145790ae5dc86658de0aa2391ed
Edson Gabriel Jacques <egmoreira10@gmail.com>
2023-10-14 14:27:14 -0300

    Fix: MySQL connection exemple

---
+++ backend/mysql_conn_exemple.php
@@ -0,0 +1,9 @@
+<?php
+
+# Mude os valores com base no seu banco de dados
+$servername = 'HOST';
+$username = 'USER';
+$password = 'PASSWORD';
+$dbname = 'DATABASE';
+
+$mysqli = new mysqli($servername, $username, $password, $dbname);
